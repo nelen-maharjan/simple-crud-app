3602b33b06f389f75240f40ca756b7967b771dc0
Nelen Maharjan <neleen11@gmail.com>
2024-05-07 19:52:37 +0545

    crud-app-backend

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
+const productRoute = require('./routes/product-route.js');
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+
+// routes
+app.use("/api/products", productRoute);
+
+
+app.get("/", (req, res) => {
+    res.send("Hello from Node API Server Updated");
+});
+
+
+mongoose
+    .connect(
+        "mongodb+srv://nelenmaharjan512:tXkDpE6smr6C686U@backenddb.mzbz4a8.mongodb.net/?retryWrites=true&w=majority&appName=BackendDB"
+    )
+    .then(() => {
+        console.log("Connected to database!");
+        app.listen(3001, () => {
+            console.log("Server is running on port 3000");
+        });
+    })
+    .catch(() => {
+        console.log("Connection failed!");
+    });
